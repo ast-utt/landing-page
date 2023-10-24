2400a727a23451d3efbefc2a5ccaeffaba9a1fa2
HangBT <hangbt.442664@gmail.com>
2023-10-24 17:33:46 +0700

    feat: remove nav

--- src/layouts/components/MainLayout/index.tsx
+++ src/layouts/components/MainLayout/index.tsx
@@ -1,6 +1,6 @@
+import useTrans from '@/hooks/useTrans';
 import { CommonLayout } from '@/types/layouts';
 import { Header } from '../Header';
-import useTrans from '@/hooks/useTrans';
 
 export const MainLayout = ({ children }: CommonLayout) => {
   const trans = useTrans();
@@ -9,7 +9,7 @@ export const MainLayout = ({ children }: CommonLayout) => {
     // w-full max-w-screen-xl relative mx-auto
     <>
       <Header />
-      <main className="font-sans min-h-[calc(100vh-72px)] bg-light-orange">
+      <main className="font-sans min-h-[calc(100vh-72px)] bg-orange-extra-light">
         {children}
         {/* <div className="font-volkhov">{trans.club.name}</div> */}
       </main>
